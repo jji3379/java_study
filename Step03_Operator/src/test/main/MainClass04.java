c23f3e39918dc53267ecc68ef92138f2f171909d
jeon jun ik <jji3379@naver.com>
2020-11-19 12:23:34 +0900

    or, and, not

--- Step03_Operator/src/test/main/MainClass04.java
+++ Step03_Operator/src/test/main/MainClass04.java
@@ -3,8 +3,28 @@ package test.main;
 public class MainClass04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// or 연산 
+		boolean result1 = false || false; //false
+		boolean result2 = false || true; //true
+		boolean result3 = true || false; //true
+		boolean result4 = true || true; //true
+		// and 연산
+		boolean result5 = false && false; //false
+		boolean result6 = false && true; //false
+		boolean result7 = true && false; //false
+		boolean result8 = true && true; //true
+		// not 연산
+		boolean result9 = !true; //false
+		boolean result19 = !false; //true
+		
+		boolean isRun=false;
+		if(isRun != true){
+			System.out.println("달리지 않아요");
+		}
+		
+		if(!isRun){
+			System.out.println("달리지 않아요2");
+		}
 	}
 
 }
