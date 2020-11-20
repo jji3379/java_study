62936075b837453d1656f670273268086839fa7d
jeon jun ik <jji3379@naver.com>
2020-11-20 17:55:44 +0900

    static, non static

--- Step04_Class/src/test/mypac/MyUtil.java
+++ Step04_Class/src/test/mypac/MyUtil.java
@@ -3,9 +3,15 @@ package test.mypac;
  *	static 자원을 포함하는 용도로 사용해 보기
  */
 public class MyUtil {
+	//non static 필드
+	public int version;
 	
-	public static String owner="김구라";
+	//static 필드
+	public static String owner;
+	
 	public static void playMusic() {
 		System.out.println("음악을 틀어요!");
+		int a=this.version; // static 영역에서 static 아닌 친구 못부름
+		String c=MyUtil.owner;
 	}
 }
