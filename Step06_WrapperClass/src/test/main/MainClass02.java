2b9f52fe267d95ba244c832f38ba10869f757d63
jeon jun ik <jji3379@naver.com>
2020-11-23 17:41:00 +0900

    Step06 참조 data type(Integer, Character), toString(), parseInt()

---
+++ Step06_WrapperClass/src/test/main/MainClass02.java
@@ -0,0 +1,15 @@
+package test.main;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		double num1=10.1;
+		Double num2=10.2;
+		
+		double result1=num2+10;
+		double result2=num1+num2;
+		Double result3=num1+num2;
+		
+		String result4=num2.toString(); 			//10.2 => "10.2"
+		double result5=Double.parseDouble("99.9");  //"99.9" => 99.9
+	}
+}
