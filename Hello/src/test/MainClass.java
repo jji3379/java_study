b5e25c814a7a95451db4e7c1f39993e200e258c8
Jeon Jun Ik <jji3379@naver.com>
2020-11-23 22:38:06 +0900

    hello

--- Hello/src/test/MainClass.java
+++ Hello/src/test/MainClass.java
@@ -1,11 +1,8 @@
 package test;
-
-// MainClass 라는 이름의 클래스 
+//MainClass 라는 이름의 클래스
 public class MainClass {
-	// main 이라는 이름의 메소드(함수)
+	//main 이라는 이름의 메소드(함수)
 	public static void main(String[] args) {
-		
 		System.out.println("Hello, World!");
-		
 	}
 }
--- Hello/src/test/MainClass02.java
+++ Hello/src/test/MainClass02.java
@@ -1,7 +1,7 @@
 package test;
 
 public class MainClass02 {
-	// run 했을때 실행의 흐름이 시작되는 특별한 main 메소드 
+	//run 했을 때 실행의 흐름이 시작되는 특별한 main 메소드
 	public static void main(String[] args) {
 		System.out.println("하나");
 		System.out.println("두울");
