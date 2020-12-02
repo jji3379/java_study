06415b157035db2eb8e485ec3cc47d0a459267b7
Jeon Jun Ik <jji3379@naver.com>
2020-12-02 17:55:02 +0900

    Thread sleep

---
+++ Step15_Exception/src/test/main/MainClass06.java
@@ -0,0 +1,17 @@
+package test.main;
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) {
+			//for 문 안에서 1초씩 delay 시키는 예제
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+		System.out.println("main 메소드가 종료 됩니다.");
+	}
+}
