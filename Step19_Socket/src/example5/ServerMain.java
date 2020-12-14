2c12acdf43672b4e41a4a5d2ded024d998cad35c
jeon jun ik <jji3379@naver.com>
2020-12-14 14:33:08 +0900

    Socket 예제 : 서버 연결 채팅

---
+++ Step19_Socket/src/example5/ServerMain.java
@@ -0,0 +1,93 @@
+package example5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerMain {
+	//접속한 클라이언트를 응대하는 스레드 객체의 목록을 담을 필드
+	static List<ServerThread> threadList;
+	
+	public static void main(String[] args) {
+		//ArrayList 객체 생성해서 필드에 저장하기
+		threadList=new ArrayList<>();
+		ServerSocket serverSocket=null;
+		try {
+			//5000 번 통신 port 를 열고 클라이언트의 접속을 기다린다.
+			serverSocket=new ServerSocket(5000);
+			/*
+			 *  .accept() 메소드는 클라이언트가 접속을 해야지 리턴하는 메소드이다.
+			 *  클라이언트가 접속을 해오면 해당 클라이언트와 연결된 Socket 객체의
+			 *  참조값을 리턴한다. 
+			 */
+			while(true) {
+				System.out.println("클라이언트의 Socket 접속을 기다립니다...");
+				//1. 클라이언트가 접속을 해오면
+				Socket socket=serverSocket.accept();
+				//2. 새로운 스레드를 시작 시킨다.
+				ServerThread t=new ServerThread(socket);
+				t.start();
+				//3. 시작된 스레드의 참조값을 목록에 누적 시킨다.
+				threadList.add(t);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("ServerMain main 메소드가 종료 됩니다.");
+	}//main END
+	
+	//스레드 클래스 설계
+	static class ServerThread extends Thread{
+		//필드 (클라이언트와 연결된 Socket 객체의 참조값을 저장할 필드)
+		private Socket socket;
+		private BufferedWriter bw;
+		//생성자
+		public ServerThread(Socket socket) {
+			//생성자의 인자로 전달받은 Socket 객체의 참조값을 필드에 저장하기
+			this.socket=socket;
+		}//생성자 END
+		
+		//인자로 전달되는 문자열을 Socket 객체를 통해서 출력하는 메소드
+		public void broadcast(String msg) throws IOException {
+			bw.write(msg); //throw 해도 밑에서 try catch 하고 있어서 괜찮음
+			bw.newLine();
+			bw.flush();
+		}
+		
+		@Override
+		public void run() {
+			try {
+				//InputStream is=socket.getInputStream();
+				//InputStreamReader isr = new InputStreamReader(is);
+				//BufferedReader br = new BufferedReader(isr);
+				
+				//클라이언트가 전송하는 문자열을 읽어들일 객체
+				BufferedReader br=
+						new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				//클라이언트에게 문자열을 출력할 객체
+				bw=
+						new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+				//반복문 돌면서 클라이언트가 전송하는 문자열이 있는지 읽어와 본다.
+				while(true) {
+					//문자열 한줄이 전송될 때 까지 블록킹 되는 메소드
+					String line=br.readLine(); //클라이언트가 전송한 문자열을 읽어와서
+					//반복문 돌면서 모든 클라이언트에게 전송하기
+					for(ServerThread tmp: threadList) {
+						tmp.broadcast(line);
+					}//run_for END
+				}//run_while END
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}//catch END
+		}//run overrid END
+	}//ServerThread Class END
+}
+
+
